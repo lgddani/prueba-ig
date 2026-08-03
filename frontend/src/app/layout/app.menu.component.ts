ce69f338d65b718b317ad104d7af6783b7871370
Daniel Enriquez <rdanienriquez01@gmail.com>
2026-08-03 11:14:43 -0500

    scaffold inicial Angular + PrimeNG con plantilla Sakai

---
+++ frontend/src/app/layout/app.menu.component.ts
@@ -0,0 +1,25 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LayoutService } from './service/app.layout.service';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[] = [];
+
+    constructor(public layoutService: LayoutService) { }
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Gestión de proyectos',
+                items: [
+                    { label: 'Proyectos', icon: 'pi pi-fw pi-th-large', routerLink: ['/proyectos'] }
+                ]
+            }
+        ];
+    }
+}
---
+++ frontend/src/app/layout/app.topbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { LayoutService } from "./service/app.layout.service";
+import { AuthService } from '../core/auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-topbar',
+    templateUrl: './app.topbar.component.html'
+})
+export class AppTopBarComponent {
+
+    items!: MenuItem[];
+
+    @ViewChild('menubutton') menuButton!: ElementRef;
+
+    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+
+    @ViewChild('topbarmenu') menu!: ElementRef;
+
+    constructor(
+        public layoutService: LayoutService,
+        public auth: AuthService,
+        private readonly router: Router
+    ) { }
+
+    cerrarSesion(): void {
+        this.auth.logout();
+        this.router.navigate(['/auth/login']);
+    }
+}
